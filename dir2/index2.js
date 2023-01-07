81a177d2fe067312755ca408b4f12fba0d9ba625
kanupriyabasoya <kanupriyakenny@gmail.com>
2023-01-07 22:11:51 +0530

    commit

--- dir2/index2.js
+++ dir2/index2.js
@@ -1,3 +1,9 @@
-//api's
-//fetch(url).then(res=>res.json).then(data=>data);
-//make object
+ document.querySelector('button').addEventListener('click',getFetch);
+ function getFetch(){
+    const choice = document.querySelector('input').value;
+    const url = `https://www.dnd5eapi.co/api/spells/${choice}/`;
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>console.log(data))
+    .catch(err=>console.log(`error${err}`));
+ };
