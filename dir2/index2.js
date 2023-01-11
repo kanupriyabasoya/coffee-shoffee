e4e52c75ee5413f2c45ac66116618ee0ecc25cdd
kanupriyabasoya <kanupriyakenny@gmail.com>
2023-01-11 22:46:03 +0530

    commit

--- dir2/index2.js
+++ dir2/index2.js
@@ -1,1 +1,18 @@
 //local storage
+
+// function getFetch(){
+
+// fetch('http://openlibrary.org/subjects/love.json?published_in=1500-1600')
+// .then(response => response.json())
+// .then(data => {
+//    data.works.forEach(element => {
+//       const li = document.createElement('li');
+//       li.textContent = element.title;
+//       document.querySelector('ul').appendChild(li);
+//    });
+   
+//    console.log(data.works)
+// });
+// }
+
+// document.querySelector('button').addEventListener('click', getFetch);
