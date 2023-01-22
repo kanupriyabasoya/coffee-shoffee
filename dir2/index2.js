497632db1b97f7f6009296a93d7e303234ae7d94
kanupriyabasoya <kanupriyakenny@gmail.com>
2023-01-22 20:38:25 +0530

    commit

--- dir2/index2.js
+++ dir2/index2.js
@@ -1,26 +1,41 @@
-//local storage
+//1.fetching nasa latest image using it's api
 
 // function getFetch(){
-    document.querySelector('button').addEventListener('click', getvalue);
+//     document.querySelector('button').addEventListener('click', getvalue);
 
-    function getvalue(){
-        fetch('https://api.nasa.gov/planetary/apod?api_key=7RsqsXc7yNHN8BZSbSF7tMKSMApGITOTLMNF0VvK')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
+//     function getvalue(){
+//         fetch('https://api.nasa.gov/planetary/apod?api_key=7RsqsXc7yNHN8BZSbSF7tMKSMApGITOTLMNF0VvK')
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
         
-            const h2 = document.querySelector('h2');
-            const para = document.querySelector('p');
-            const img = document.querySelector('img');
+//             const h2 = document.querySelector('h2');
+//             const para = document.querySelector('p');
+//             const img = document.querySelector('img');
 
-            h2.textContent = data.date;
-            para.textContent = data.explanation;
-            img.src = data.hdurl;
-        });
+//             h2.textContent = data.date;
+//             para.textContent = data.explanation;
+//             img.src = data.hdurl;
+//         });
 
-}
+// }
    
-   
+   //2.making class with 3key and two methods
+
+//    class MakeCar{
+//     constructor(carName,carColor,carModel){
+//         this.carName = carName;
+//         this.carColor = carColor;
+//         this.carModel = carModel;
+//     }
+
+//     carInfo(){
+//         console.log(this.carName, this.carColor, this.carModel);
+//     }
+//    }
+
+//    const tesla = new MakeCar("tesla","black","alpha");
+//    console.log(tesla);
  
  
 
